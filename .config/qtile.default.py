7df29b843f278b26dc3e190214aaa737d809d984
phraxi <jannikhodes@gmx.de>
2023-04-30 15:07:39 +0200

    backups from /.config and other config files

---
+++ .config/qtile.default.py
@@ -0,0 +1,275 @@
+# Copyright (c) 2010 Aldo Cortesi
+# Copyright (c) 2010, 2014 dequis
+# Copyright (c) 2012 Randall Ma
+# Copyright (c) 2012-2014 Tycho Andersen
+# Copyright (c) 2012 Craig Barnes
+# Copyright (c) 2013 horsik
+# Copyright (c) 2013 Tao Sauvage
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from libqtile import bar, layout, widget
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+from qtile_extras import widget
+
+#colors
+NEONGREEN= '#00ff00'
+YELLOW= ' #ffed82'
+GREY= '#c8caca' 
+MAGENTA = '#e71fd9'
+PINK = '#ff7be1' 
+LAVANDER='#bd77be' 
+
+
+mod = "mod4"
+terminal = guess_terminal()
+
+keys = [
+    # A list of available commands that can be bound to keys can be found
+    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
+    # Switch between windows
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "Tab", lazy.layout.next(), desc="Move window focus to other window"),
+    # Move windows between left/right columns or move up/down in current stack.
+    # Moving out of range in Columns layout will create new column.
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    # Grow windows. If current window is on the edge of screen and direction
+    # will be to screen edge - window would shrink.
+    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key(
+        [mod, "shift"],
+        "Return",
+        lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack",
+    ),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    # Toggle between different layouts as defined below
+    Key([mod], "space", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    
+    #custom Keys for Volume control
+    Key([mod], "F6", lazy.spawn("amixer -q sset Master toggle"),desc="Toggle Mute" ),
+    Key([mod], "F7", lazy.spawn("amixer -q sset Master 5%-"),desc="Decrease Volume"),
+    Key([mod], "F8", lazy.spawn("amixer -q sset Master 5%+"),desc="Increase Volume"),
+
+    #Custom Keys for Brightness control
+    Key([mod], "F2", lazy.spawn("xbacklight -dec 10"),desc="Toggle Mute" ),
+    Key([mod], "F3", lazy.spawn("xbacklight -inc 10"),desc="Toggle Mute" ),
+
+    #Some more Custom Bindings
+    Key([mod], "a", lazy.spawn("rofi -show drun"),desc="launch application from rofi" ),
+]
+
+
+groups = [
+        Group("1",
+            matches=[Match(wm_class=["code-oss"])], 
+            label="DEV"),
+        Group("2", 
+            matches=[Match(wm_class=["texmaker","libreoffice"])],
+            label="UNI"),
+        Group("3",
+            #spawn="firefox", 
+            label="WWW"),
+        Group("4",
+            matches=[Match(wm_class=["pcmanfm"])],
+            label="SYS"),
+]
+
+for i in groups:
+    keys.extend(
+        [
+            # mod1 + letter of group = switch to group
+            Key(
+                [mod],
+                i.name,
+                lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name),
+            ),
+            # mod1 + shift + letter of group = switch to & move focused window to group
+            Key(
+                [mod, "shift"],
+                i.name,
+                lazy.window.togroup(i.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(i.name),
+            ),
+            # Or, use below if you prefer not to switch to that group.
+            # # mod1 + shift + letter of group = move focused window to group
+            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+            #     desc="move focused window to group {}".format(i.name)),
+        ]
+    )
+
+layouts = [
+    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.Max(),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.Matrix(),
+    # layout.MonadTall(),
+    # layout.MonadWide(),
+    # layout.RatioTile(),
+    # layout.Tile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
+
+widget_defaults = dict(
+    font="sans",
+    fontsize=12,
+    padding=3,
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        wallpaper='~/.config/qtile/wallpapers/edgerunners02.jpg',
+        wallpaper_mode = 'center',
+        bottom=bar.Bar(
+            [
+                widget.CurrentLayout(foreground='#e71fd9'),
+                widget.GroupBox(
+                    highlight_method='line', 
+                    active='#e71fd9', 
+                    inactive= '#e71fd9',
+                    hide_unused=False),
+                widget.Prompt(foreground='#e71fd9'),
+                widget.WindowName(foreground='#e71fd9'),
+                #bluetooth needs to be set up
+                #widget.Bluetooth(),
+                #widget.CPUGraph(),
+                #widget.Cmus(), for music gui
+                widget.Wlan(foreground=NEONGREEN),
+                widget.Battery(
+                    format='Bat: {percent:2.0%}',
+                    foreground='#00ff00'),
+                widget.Backlight(
+                    backlight_name="amdgpu_bl0", 
+                    brightness_file="/sys/class/backlight/amdgpu_bl0/brightness" ,
+                    format='Bright: {percent:2.0%}',
+                    foreground='#00ff00'),
+                widget.Clock(
+                    format="%d-%m-%Y %a %I: %M %p",
+                    foreground='#00ff00')
+            ],
+            24,
+             #border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+             #border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+        ),
+        top=bar.Bar(
+            [
+                
+                #widget.StatusNotifier(),
+                widget.Systray(),
+                widget.CPU(foreground='#e71fd9') ,
+                widget.Memory(measure_mem='M',foreground='#e71fd9'),
+                #widget.AnalogueClock(),
+                widget.BrightnessControl(
+                    brightness_path='/sys/class/backlight/amdgpu_bl0/brightness',
+                    device='/sys/class/backlight/amdgpu_bl0'), 
+                widget.ALSAWidget(
+                    bar_colour_high = MAGENTA,
+                    bar_colour_loud= MAGENTA,
+                    bar_colour_mute=GREY,
+                    bar_colour_normal= MAGENTA,
+                    mode='bar',
+                    update_interval=0.5
+                    ),
+                #widget.PulseVolume(),
+                #widget.LaunchBar(progs=['firefox', 'lau'],text_only=True),
+                #widget.CheckUpdates()
+                #widget.DF(warn_space=40), 
+                #bluetooth needs to be set up
+                #widget.Bluetooth(),
+                #widget.CPUGraph(),
+                #widget.Cmus(), for music gui
+                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
+            ],
+            24,
+             #border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+             #border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+        ),
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: list
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+# When using the Wayland backend, this can be used to configure input devices.
+wl_input_rules = None
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
